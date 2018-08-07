074d99151842f1cc587303d2284cc2abd6d84ffd
Corban Swain <CorbanSwain@gmail.com>
2018-08-07 16:48:32 -0400

    updatin init with platform specific setting

--- init.sh
+++ init.sh
@@ -11,6 +11,16 @@ EMACS_LOCAL_FILE="$(realpath ~/.emacs_local.el)"
 sed -i -e "s#{{.emacs}}#${SETTINGS_DIR}\/.emacs#g" ~/.emacs
 sed -i -e "s#{{.emacs_local}}#${EMACS_LOCAL_FILE}#g" ~/.emacs
 
+PLATFORM='unknown'
+UNAMESTR=`uname`
+if [[ $UNAMESTR == 'MINGW64_NT-10.0' ]]; then
+    PLATFORM='git_bash'
+fi
+
+if [[ $PLATFORM == 'git_bash' ]]; then
+    sed -i -r -e  's/"\/(c)\//"\1:/g' ~/.emacs
+fi
+
 bash $SETTINGS_DIR/init_git.sh
 
 source ~/.bashrc
